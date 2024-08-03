cb89df13ffdd7ec637e8766f10b6c3968470aa77
williamdrg <123426425+williamdrg@users.noreply.github.com>
2024-08-02 20:38:30 -0500

    agregar un archivo redirect

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import {  RouterProvider } from 'react-router-dom'
 import router from './Routes'
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
